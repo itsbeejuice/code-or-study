f87e4994872201e4c3b37a99f3719fb1a79ba0a6
Bee (Aka. Outlaw) <77043377+itsbeejuice@users.noreply.github.com>
2021-02-27 11:50:02 -0800

    Add files via upload

---
+++ codevstudy.py
@@ -0,0 +1,12 @@
+import random 
+
+again=True
+
+while again:
+    print("Your answer issss: ")
+    print(random.choice(['code','study']))
+    anotherroll=input("Wanna go again? ")
+    if anotherroll.lower()=='yes'or anotherroll.lower()=='y':
+        continue
+    elif anotherroll.lower() != "y" or anotherroll.lower()!="yes":
+        break 
